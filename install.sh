b8f2505e609eadf03f80407c9e83a11464cf3a98
Brandon Cornejo <brandon.cornejo@gmail.com>
2013-03-29 13:43:21 -0500

    dwm and conky configs

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 dir=~/dotfiles
-files="vimrc tmux.conf"
+files="vimrc tmux.conf conkyrc"
 
 cd $dir
 for file in $files; do
 	ln -s $dir/$file ~/.$file
 done
+
+chmod +x dwmstart
+ln -s $dir/dwmstart ~/dwmstart
 
 if [ ! -d ~/.vim/bundle/vundle ]; then
 	git clone https://github.com/gmarik/vundle.git  ~/.vim/bundle/vundle
